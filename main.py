6e85ee3b613041b0a2fd62071130eafd666b96c8
Shagun Sodhani <sshagunsodhani@gmail.com>
2017-11-27 16:35:03 -0500

    updated readme

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string(DATASET_NAME, CORA, "Name of the dataset. Supported values are cora")
-flags.DEFINE_string(MODEL_NAME, GCN_POLY, "Name of the model. Supported values are ff, gcn, gcn_poly")
+flags.DEFINE_string(MODEL_NAME, GCN, "Name of the model. Supported values are ff, gcn, gcn_poly")
 flags.DEFINE_float(LEARNING_RATE, 0.01, "Initial learning rate")
-flags.DEFINE_integer(EPOCHS, 20, "Number of epochs to train for")
+flags.DEFINE_integer(EPOCHS, 200, "Number of epochs to train for")
 flags.DEFINE_integer(HIDDEN_LAYER1_SIZE, 16, "Number of nodes in the first hidden layer")
 flags.DEFINE_float(DROPOUT, 0.5, "Dropout rate")
 flags.DEFINE_float(L2_WEIGHT, 5e-4, "Weight for L2 regularization")
-flags.DEFINE_integer(EARLY_STOPPING, 200, "Number of epochs for early stopping")
+flags.DEFINE_integer(EARLY_STOPPING, 20, "Number of epochs for early stopping")
 flags.DEFINE_string(DATA_DIR, "/Users/shagun/projects/pregel/data", "Base directory for reading the datasets")
 flags.DEFINE_bool(SPARSE_FEATURES, True, "Boolean variable to indicate if the features are sparse or not")
-flags.DEFINE_bool(POLY_DEGREE, 3, "Degree of the Chebyshev Polynomial. This value is used only if gcn_poly model is used.")
+flags.DEFINE_bool(POLY_DEGREE, 1, "Degree of the Chebyshev Polynomial. This value is used only if gcn_poly model is used.")
 flags.DEFINE_string(TENSORBOARD_LOGS_DIR, "/Users/shagun/projects/pregel/logs/tensorboard/",
                     "Directory for saving tensorboard logs")
 
